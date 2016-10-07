51b99554bea50f467c962fc44d72fc9d4808426f
Abhishek Shrivastava <abhishek.shrivastava.ts>
2016-10-07 15:46:07 +0530

    Added readme and HELLO project

---
+++ Hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+	    string name = Console.ReadLine();
+	    Console.WriteLine("Hello "+name);
+        }
+    }
+}
